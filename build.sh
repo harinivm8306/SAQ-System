09d3915a7f2b3477941ce7ac5eddf87e7328fb81
Harini V M <harinivm77@gmail.com>
2026-04-02 22:18:23 +0530

    Auto-run migrations on deployment

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Building project..."
 python3.9 -m pip install -r requirements.txt
+python3.9 manage.py migrate --noinput
 python3.9 manage.py collectstatic --noinput --clear
 echo "Build complete."
